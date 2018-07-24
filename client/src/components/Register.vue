3f1c4d049c87c91e58238be27aca0deadb939d47
yovelsapir <yovelsapir@gmail.com>
2018-07-25 01:50:19 +0300

    adding the registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input type='email' v-model="email" name='email' placeholder='email' />
+
+    <input type='password' v-model="password" name='email' placeholder='password' />
+
+    <br>
+
+    <button @click="register">Register</button>
+
+  </div>
+</template>
+
+<script>
+  import AuthenticationService from '@/services/AuthenticationService'
+  export default {
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      async register() {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        });
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,14 @@ const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'Hello World!'
+    message: `Hello: ${req.body.email} Your user was registered! have fun.`
   })
 })
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 8082)
